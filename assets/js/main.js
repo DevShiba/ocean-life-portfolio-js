b245877bbe1260584109de423cd336c79ac0926f
DevShiba <migerushiba@gmail.com>
2023-09-30 22:05:15 -0300

    Media queries, JavaScript, and other fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,12 +33,35 @@ navLink.forEach((link) => link.addEventListener("click", removeMenu));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () => {
   const header = document.getElementById("header");
-
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
   this.scrollY >= 50
     ? header.classList.add("scroll-header")
     : header.classList.remove("scroll-header");
 };
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
